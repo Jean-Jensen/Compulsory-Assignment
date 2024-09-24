f196c762b0993c116a6249217651f2a37fb078ff
Jean-Jensen <jeanjensen911@gmail.com>
2024-09-25 04:22:27 +0700

    one of these days...

--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5791325aed35ec64e9114062ebad37d3d47ab3b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72e7fe173e360271f850ca137772895a1cc93f31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
